88f2516dd2b53719e6466e014e8ec90f6a95bcb5
Federico Pintaluba <pintalubaf@gmail.com>
2020-05-20 13:19:39 -0300

    Created Constants class with base url and api key constants.

---
+++ app/src/main/java/com/federicopintaluba/popularmovies/Constants.java
@@ -0,0 +1,7 @@
+package com.federicopintaluba.popularmovies;
+
+public class Constants {
+
+    static final String BASE_URL = "https://api.themoviedb.org";
+    static final String API_KEY = "insert_yours_here";
+}
--- app/src/main/java/com/federicopintaluba/popularmovies/MainActivity.java
+++ app/src/main/java/com/federicopintaluba/popularmovies/MainActivity.java
@@ -1,14 +1,19 @@
 package com.federicopintaluba.popularmovies;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.movies_recycler_view);
     }
 }
